75b69e59b9df499d5d58eaaf644da6470e9c30ab
Rohit Venkat <rohitrvenkat@gmail.com>
2022-02-05 13:36:46 -0600

    Finish part 2 and bonus questions

--- prescribers.sql
+++ prescribers.sql
@@ -1,3 +1,5 @@
+-- PART 1
+
 -- 1a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 SELECT 
 	npi,
@@ -10,8 +12,7 @@ ORDER BY total_claims DESC;
 
 -- 1b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name, specialty_description, and the total number of claims.
 SELECT
-	nppes_provider_first_name, 
-	nppes_provider_last_org_name,
+	nppes_provider_first_name || ' ' || nppes_provider_last_org_name,
 	specialty_description,
 	total_claims
 FROM prescriber
@@ -46,18 +47,16 @@ ORDER BY total_claims DESC;
 -- 2b. Which specialty had the most total number of claims for opioids?
 SELECT 
 	specialty_description,
-	SUM(opioid_claims) as opioid_claims
+	SUM(total_claim_count) as opioid_claims
 FROM prescriber
-INNER JOIN (
-	SELECT 
-		npi, 
-		SUM(bene_count) AS opioid_claims
-	FROM prescription
-	GROUP BY npi ) AS prescription
-	USING(npi)
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING(drug_name)
+WHERE opioid_drug_flag = 'Y'
 GROUP BY specialty_description
-ORDER BY opioid_claims DESC NULLS LAST;
--- Family Practice (2,019,490 total claims for opioids)
+ORDER BY opioid_claims DESC;
+-- Nurse Practitioner (900,845 total claims for opioids)
 
 
 -- 2c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
@@ -79,13 +78,29 @@ HAVING SUM(total_claims) IS NULL;
 -- 2d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
 SELECT 
 	specialty_description,
-	ROUND(COALESCE(SUM(bene_count), 0) / SUM(total_claim_count) * 100, 2) AS opioid_claims_pct
-FROM prescriber
-INNER JOIN prescription
+	ROUND(opioid_claims / total_claims * 100, 2) AS opioid_claims_pct
+FROM (
+	SELECT 
+		specialty_description,
+		SUM(total_claim_count) AS opioid_claims
+	FROM prescription
+	INNER JOIN prescriber
 	USING(npi)
-GROUP BY specialty_description
+	INNER JOIN drug
+	USING(drug_name)
+	WHERE opioid_drug_flag = 'Y'
+	GROUP BY specialty_description ) AS opioids
+INNER JOIN (
+	SELECT
+		specialty_description,
+		SUM(total_claim_count) AS total_claims
+	FROM prescription
+	INNER JOIN prescriber
+	USING (npi)
+	GROUP BY specialty_description ) AS all_drugs
+USING (specialty_description)
 ORDER BY opioid_claims_pct DESC;
--- Oral surgeons have the highest percentage of total claims that are for opioids (81.02-83.74%).
+-- Case Manager/Care Coordinator has the highest percentage of total claims that are for opioids (72.00%).
 
 
 -- 3a. Which drug (generic_name) had the highest total drug cost?
@@ -251,3 +266,349 @@ LEFT JOIN prescription
 WHERE specialty_description = 'Pain Management'
 AND nppes_provider_city = 'NASHVILLE'
 AND opioid_drug_flag = 'Y';
+
+
+-- PART 2
+
+-- 1. How many npi numbers appear in the prescriber table but not in the prescription table?
+SELECT COUNT(DISTINCT NPI)
+FROM PRESCRIBER
+WHERE NPI NOT IN
+		(SELECT DISTINCT NPI
+		 FROM PRESCRIPTION)
+-- 4458 NPIs
+
+
+-- 2a. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Family Practice.
+SELECT 
+	generic_name,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+using(drug_name)
+WHERE specialty_description = 'Family Practice'
+GROUP BY generic_name
+ORDER BY total_claims DESC
+LIMIT 5;
+
+
+ -- 2b. Find the top five drugs (generic_name) prescribed by prescribers with the specialty of Cardiology.
+SELECT 
+	generic_name,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+using(drug_name)
+WHERE specialty_description = 'Cardiology'
+GROUP BY generic_name
+ORDER BY total_claims DESC
+LIMIT 5;
+
+
+-- 2c. Which drugs appear in the top five prescribed for both Family Practice prescribers and Cardiologists? Combine what you did for parts a and b into a single query to answer this question.
+	(SELECT generic_name
+	 FROM prescriber
+	 INNER JOIN prescription USING(npi)
+	 INNER JOIN drug USING(drug_name)
+	 WHERE specialty_description = 'Family Practice'
+	 GROUP BY generic_name
+	 ORDER BY SUM(total_claim_count) DESC
+	 LIMIT 5) 
+INTERSECT
+	(SELECT generic_name
+	 FROM prescriber
+	 INNER JOIN prescription USING(npi)
+	 INNER JOIN drug USING(drug_name)
+	 WHERE SPECIALTY_DESCRIPTION = 'Cardiology'
+	 GROUP BY generic_name
+	 ORDER BY SUM(total_claim_count) DESC
+	 LIMIT 5);
+
+
+-- 3a. Your goal in this question is to generate a list of the top prescribers in each of the major metropolitan areas of Tennessee. First, write a query that finds the top 5 prescribers in Nashville in terms of the total number of claims (total_claim_count) across all drugs. Report the npi, the total number of claims, and include a column showing the city.
+SELECT 
+	npi,
+	SUM(total_claim_count) as total_claims,
+	nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+WHERE nppes_provider_city = 'NASHVILLE'
+GROUP BY npi, nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+
+
+-- 3b. Now, report the same for Memphis.
+SELECT 
+	npi,
+	SUM(total_claim_count) as total_claims,
+	nppes_provider_city
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+WHERE nppes_provider_city = 'MEMPHIS'
+GROUP BY npi, nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+
+
+-- 3c. Combine your results from a and b, along with the results for Knoxville and Chattanooga.
+	(SELECT 
+ 		npi,
+		SUM(total_claim_count) AS total_claims,
+		nppes_provider_city
+	FROM prescriber
+	INNER JOIN prescription 
+	USING(npi)
+	WHERE nppes_provider_city = 'NASHVILLE'
+	GROUP BY npi, nppes_provider_city
+	ORDER BY total_claims DESC
+	LIMIT 5)
+UNION
+	(SELECT 
+ 		npi,
+		SUM(total_claim_count) AS total_claims,
+		nppes_provider_city
+	FROM prescriber
+	INNER JOIN prescription 
+	USING(npi)
+	WHERE nppes_provider_city = 'MEMPHIS'
+	GROUP BY npi, nppes_provider_city
+	ORDER BY total_claims DESC
+	LIMIT 5)
+UNION
+	(SELECT 
+	 	npi,
+		SUM(total_claim_count) AS total_claims,
+		nppes_provider_city
+	FROM prescriber
+	INNER JOIN prescription 
+	USING(npi)
+	WHERE nppes_provider_city = 'KNOXVILLE'
+	GROUP BY npi, nppes_provider_city
+	ORDER BY total_claims DESC
+	LIMIT 5)
+UNION
+	(SELECT 
+	 	npi,
+		SUM(total_claim_count) AS total_claims,
+		nppes_provider_city
+	FROM prescriber
+	INNER JOIN prescription 
+	USING(npi)
+	WHERE nppes_provider_city = 'CHATTANOOGA'
+	GROUP BY npi, nppes_provider_city
+	ORDER BY total_claims DESC
+	LIMIT 5)
+ORDER BY total_claims DESC;
+
+
+--4. Find all counties which had an above-average (for the state) number of overdose deaths in 2017. Report the county name and number of overdose deaths.
+SELECT 
+	county,
+	overdose_deaths
+FROM overdose_deaths
+INNER JOIN fips_county 
+USING(fipscounty)
+WHERE year = 2017
+	AND overdose_deaths >
+		(SELECT AVG(overdose_deaths)
+			FROM overdose_deaths
+			WHERE year = 2017)
+ORDER BY overdose_deaths DESC;
+
+
+-- 5a. Write a query that finds the total population of Tennessee.
+SELECT SUM(population) AS total_population
+FROM population
+-- Total Population of Tennessee: 6,597,381
+ 
+ 
+-- 5b. Build off of the query that you wrote in part a to write a query that returns for each county that county's name, its population, and the percentage of the total population of Tennessee that is contained in that county.
+SELECT
+	county,
+	ROUND(population / SUM(population) OVER() * 100, 2) AS population_pct
+FROM population
+INNER JOIN fips_county
+USING (fipscounty)
+ORDER BY population_pct DESC;
+
+
+-- BONUS
+
+-- In this set of exercises you are going to explore additional ways to group and organize the output of a query when using postgres. 
+
+-- For the first few exercises, we are going to compare the total number of claims from Interventional Pain Management Specialists compared to those from Pain Managment specialists.
+
+-- 1. Write a query which returns the total number of claims for these two groups. Your output should look like this: 
+
+-- specialty_description         |total_claims|
+-- ------------------------------|------------|
+-- Interventional Pain Management|       55906|
+-- Pain Management               |       70853|
+
+SELECT 
+	specialty_description,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+GROUP BY specialty_description
+
+
+-- 2. Now, let's say that we want our output to also include the total number of claims between these two groups. Combine two queries with the UNION keyword to accomplish this. Your output should look like this:
+
+-- specialty_description         |total_claims|
+-- ------------------------------|------------|
+--                               |      126759|
+-- Interventional Pain Management|       55906|
+-- Pain Management               |       70853|
+
+	(SELECT
+	 	NULL AS specialty_description,
+	 	SUM(total_claim_count) AS total_claims
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING(npi)
+	 WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management'))
+UNION
+	(SELECT 
+	 	specialty_description,
+	 	SUM(total_claim_count) AS total_claims
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING(npi)
+	 WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+	 GROUP BY specialty_description);
+
+
+-- 3. Now, instead of using UNION, make use of GROUPING SETS (https://www.postgresql.org/docs/10/queries-table-expressions.html#QUERIES-GROUPING-SETS) to achieve the same output.
+SELECT 
+	specialty_description,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+GROUP BY GROUPING SETS(specialty_description, ())
+
+
+-- 4. In addition to comparing the total number of prescriptions by specialty, let's also bring in information about the number of opioid vs. non-opioid claims by these two specialties. Modify your query (still making use of GROUPING SETS so that your output also shows the total number of opioid claims vs. non-opioid claims by these two specialites:
+
+-- specialty_description         |opioid_drug_flag|total_claims|
+-- ------------------------------|----------------|------------|
+--                               |                |      129726|
+--                               |Y               |       76143|
+--                               |N               |       53583|
+-- Pain Management               |                |       72487|
+-- Interventional Pain Management|                |       57239|
+
+SELECT 
+	specialty_description,
+	opioid_drug_flag,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING (drug_name)
+WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+GROUP BY GROUPING SETS(opioid_drug_flag, specialty_description, ())
+
+
+-- 5. Modify your query by replacing the GROUPING SETS with ROLLUP(opioid_drug_flag, specialty_description). How is the result different from the output from the previous query?
+SELECT 
+	specialty_description,
+	opioid_drug_flag,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING (drug_name)
+WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+GROUP BY ROLLUP(opioid_drug_flag, specialty_description)
+
+
+-- 6. Switch the order of the variables inside the ROLLUP. That is, use ROLLUP(specialty_description, opioid_drug_flag). How does this change the result?
+SELECT 
+	specialty_description,
+	opioid_drug_flag,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING (drug_name)
+WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+GROUP BY ROLLUP(specialty_description, opioid_drug_flag)
+
+
+-- 7. Finally, change your query to use the CUBE function instead of ROLLUP. How does this impact the output?
+SELECT 
+	specialty_description,
+	opioid_drug_flag,
+	SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING(npi)
+INNER JOIN drug
+USING (drug_name)
+WHERE specialty_description IN ('Interventional Pain Management', 'Pain Management')
+GROUP BY CUBE(specialty_description, opioid_drug_flag)
+
+
+-- 8. In this question, your goal is to create a pivot table showing for each of the 4 largest cities in Tennessee (Nashville, Memphis, Knoxville, and Chattanooga), the total claim count for each of six common types of opioids: Hydrocodone, Oxycodone, Oxymorphone, Morphine, Codeine, and Fentanyl. For the purpose of this question, we will put a drug into one of the six listed categories if it has the category name as part of its generic name. For example, we could count both of "ACETAMINOPHEN WITH CODEINE" and "CODEINE SULFATE" as being "CODEINE" for the purposes of this question.
+
+-- The end result of this question should be a table formatted like this:
+
+-- city       |codeine|fentanyl|hyrdocodone|morphine|oxycodone|oxymorphone|
+-- -----------|-------|--------|-----------|--------|---------|-----------|
+-- CHATTANOOGA|   1323|    3689|      68315|   12126|    49519|       1317|
+-- KNOXVILLE  |   2744|    4811|      78529|   20946|    84730|       9186|
+-- MEMPHIS    |   4697|    3666|      68036|    4898|    38295|        189|
+-- NASHVILLE  |   2043|    6119|      88669|   13572|    62859|       1261|
+
+-- For this question, you should look into use the crosstab function, which is part of the tablefunc extension (https://www.postgresql.org/docs/9.5/tablefunc.html). In order to use this function, you must (one time per database) run the command
+-- 	CREATE EXTENSION tablefunc;
+
+-- Hint #1: First write a query which will label each drug in the drug table using the six categories listed above.
+-- Hint #2: In order to use the crosstab function, you need to first write a query which will produce a table with one row_name column, one category column, and one value column. So in this case, you need to have a city column, a drug label column, and a total claim count column.
+-- Hint #3: The sql statement that goes inside of crosstab must be surrounded by single quotes. If the query that you are using also uses single quotes, you'll need to escape them by turning them into double-single quotes.
+
+CREATE EXTENSION tablefunc; 
+
+SELECT *
+FROM CROSSTAB($$
+	SELECT
+		nppes_provider_city AS city,
+		CASE
+			WHEN generic_name like '%CODEINE%' THEN 'codeine'
+			WHEN generic_name like '%FENTANYL%' THEN 'fentanyl'
+			WHEN generic_name like '%HYDROCODONE%' THEN 'hyrdocodone'
+			WHEN generic_name like '%MORPHINE%' THEN 'morphine'
+			WHEN generic_name like '%OXYCODONE%' THEN 'oxycodone'
+			WHEN generic_name like '%OXYMORPHONE%' THEN 'oxymorphone'
+		END AS drug_type,
+		SUM(total_claim_count) AS total_claims
+	FROM prescriber
+	INNER JOIN prescription
+	USING(npi)
+	INNER JOIN drug
+	USING (drug_name)
+	WHERE nppes_provider_city IN ('NASHVILLE', 'MEMPHIS', 'KNOXVILLE', 'CHATTANOOGA')
+	GROUP BY city, drug_type
+	ORDER BY city, drug_type $$)
+AS (city text,
+	codeine numeric,
+	fentanyl numeric,
+	hyrdocodone numeric,
+	morphine numeric,
+	oxycodone numeric,
+	oxymorphone numeric)
